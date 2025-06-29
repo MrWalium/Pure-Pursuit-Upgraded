c414d0177afc57fafa49eb93e5b4aac0b79e90f2
unknown <coolcoderai123@gmail.com>
2025-06-29 15:40:43 -0700

    Skeleton for code

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
+# --------------- DEPENDENCIES --------------- #
+# showing the animation with matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.widgets import Button
+
+# because we love numpy
+import numpy as np
+
+# For actions executed when a button is pressed
+from functools import partial
+
+# because I'll probably need it
+import math
+
+# --------------- CLASSES --------------- #
+
+class Animation:
+    def __init__(self):
+        self. =
+
+    def
+
+class Robot:
+    def __init__(self, x:float=0, y:float=0, heading:float=90, maxVelocity:float=1, maxAcceleration:float=1, scaling:float=1):
+        self.x = x
+        self.y = y
+        self.heading = heading
+        self.maxVelocity = maxVelocity
+        self.maxAcceleration = maxAcceleration
+        # For drawing
+        # To scale the robot shown by a factor of scaling
+        self.scaling = scaling
+
+    def draw(self):
+        # do later
+
+# --------------- MAIN --------------- #
+
+# calls the functions which actually do stuff
+# the function is literally useless, but I hold onto my c++ roots
 def main():
     print("Hello World!!! :)")
 
+# I do not embrace some python aspects
 main()
+plt.close()
