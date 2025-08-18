92b88074ba66f587f4d7e472fbf5c817c563a385
MrWalium <coolcoderai123@gmail.com>
2025-08-17 17:10:46 -0700

    Finished drawing function and added velocity and acceleration logic

--- main.py
+++ main.py
@@ -101,53 +101,76 @@ class Animation:
         # Remember to update the path
         anim = animation.FuncAnimation(fig=self.fig, func=partial(updateFrame, ax=self.ax, robot=self.robot,
                                                                   trailX=self.trailX, trailY=self.trailY,
-                                                                  trail=self.trail), blit=True, cache_frame_data=False,
-                                       interval=10)
+                                                                  trail=self.trail), blit=True, cache_frame_data=False,interval=10)
 
         plt.show()
 
 
 class Robot:
-    def __init__(self, ax, x: float = 0, y: float = 0, heading: float = 0, maxVelocity: float = 0.1,
-                 maxAcceleration: float = 1, maxTurnVelocity: float = 1, scaling: float = 1, isdiffy: bool = True):
+    def __init__(self, ax, x: float = 0, y: float = 0, heading: float = 0, MAX_VELOCITY: float = 0.1,
+                 MAX_ACCELERATION: float = 1, MAX_TURN_VELOCITY: float = 3.35, MAX_TURN_ACCELERATION: float = 1,
+                 scaling: float = 1, isdiffy: bool = True):
+        """
+        Initializes all of the positional, velocity, acceleration, maximum, and draw shape variables.
+        Args:
+            ax: the axis on which the robot is drawn
+            x: inital x position of the robot
+            y: inital y postition of the robot
+            heading: inital heading of the robot
+            MAX_VELOCITY: maximum linear velocity
+            MAX_ACCELERATION: maximum linear acceleration
+            MAX_TURN_VELOCITY: maximum rate of change in velocity angle
+            scaling: how much bigger or smaller the robot is drawn
+            isdiffy: if the robot is drawn with a differential swerve or holonomic drivetrain
+        """
+        # --- instance variables --- #
         # the plot axis
         self.ax = ax
 
         # positional variables
         self.x = x
         self.y = y
-        # change in x, for drawing robot
-        self.dx = 0
-        self.dy = 0
         self.heading = heading
-        self.maxVelocity = maxVelocity
-        self.maxAcceleration = maxAcceleration
-        self.maxTurnVelocity = maxTurnVelocity
 
-        # I'm using polar coordinates for simplicity
+        # I'm using polar coordinates for velocity for simplicity
         self.velocity = 0.1
+        # how fast the velocity angle is changing
+        self.turnVelocity = 3.35
         self.velocityAngle = 0
+
+        # accelerations
+        self.acceleration = 0
+        self.turnAcceleration = 0
+
+        # maximums
+        self.MAX_VELOCITY = MAX_VELOCITY
+        self.MAX_ACCELERATION = MAX_ACCELERATION
+        self.MAX_TURN_VELOCITY = MAX_TURN_VELOCITY
+        self.MAX_TURN_ACCELERATION = MAX_TURN_ACCELERATION
 
         # for drawing
         # to scale the robot shown by a factor of scaling
         self.scaling = scaling
         self.isdiffy = isdiffy
 
-        # initializing robot shapes
+        # --- drawing variables --- #
+
+        # (x, y) positions, widths, radius's, etc. are 0 because
+        # they are going to be updated immediately in the update function anyway
 
         self.body = plt.Rectangle((0, 0), width=0, height=0, angle=self.heading,
                                   rotation_point='center', lw=None, color='darkslategrey')
         # r=right, l=left, f=front, b=back
-        self.wheelfr = plt.Rectangle((0, 0),width=0, height=0, angle=self.heading,
+        self.wheelfr = plt.Rectangle((0, 0), width=0, height=0, angle=self.heading,
                                      rotation_point='center', lw=None, color='black')
         self.wheelbr = plt.Rectangle((0, 0), width=0, height=0, angle=self.heading,
                                      rotation_point='center', lw=None, color='black')
-        self.wheelfl = plt.Rectangle((0, 0),width=0, height=0, angle=self.heading,
+        self.wheelfl = plt.Rectangle((0, 0), width=0, height=0, angle=self.heading,
                                      rotation_point='center', lw=None, color='black')
         self.wheelbl = plt.Rectangle((0, 0), width=0, height=0, angle=self.heading,
                                      rotation_point='center', lw=None, color='black')
         self.eyer = plt.Circle((0, 0), radius=0.05 * self.scaling, color='white', lw=None)
-        self.eyel = plt.Circle((0, 0), radius=0.05 * self.scaling,color='white', lw=None)
+        self.eyel = plt.Circle((0, 0), radius=0.05 * self.scaling, color='white', lw=None)
         self.pupilr = plt.Circle((0, 0), radius=0.02 * self.scaling, color='black', lw=None)
         self.pupill = plt.Circle((0, 0), radius=0.02 * self.scaling, color='black', lw=None)
 
@@ -156,24 +179,12 @@ class Robot:
         self.frontTriangle = plt.Polygon(((0, 0), (0, 0)), fill=True, color='firebrick')
 
         # so the wheels can rotate
-        self.wheelHolefr = plt.Circle(
-            (self.x + 0.24234531148 * math.cos(math.radians(self.heading + 45)) * self.scaling,
-             self.y + 0.24234531148 * math.sin(
-                 math.radians(self.heading + 45)) * self.scaling),
-            radius=0.055, color='darkgrey')
-        self.wheelHolebr = plt.Circle(
-            (self.x + 0.24234531148 * math.cos(math.radians(self.heading + 315)) * self.scaling,
-             self.y + 0.24234531148 * math.sin(math.radians(self.heading + 315)) * self.scaling),
-            radius=0.055, color='darkgrey')
-        self.wheelHolefl = plt.Circle(
-            (self.x + 0.23234531148 * math.cos(math.radians(self.heading + 133)) * self.scaling,
-             self.y + 0.23234531148 * math.sin(math.radians(self.heading + 133)) * self.scaling),
-            radius=0.055, color='darkgrey')
-        self.wheelHolebl = plt.Circle(
-            (self.x + 0.23234531148 * math.cos(math.radians(self.heading + 226)) * self.scaling,
-             self.y + 0.23234531148 * math.sin(math.radians(self.heading + 226)) * self.scaling),
-            radius=0.055, color='darkgrey')
+        self.wheelHolefr = plt.Circle((0, 0), radius=0.055, color='darkgrey')
+        self.wheelHolebr = plt.Circle((0, 0), radius=0.055, color='darkgrey')
+        self.wheelHolefl = plt.Circle((0, 0), radius=0.055, color='darkgrey')
+        self.wheelHolebl = plt.Circle((0, 0), radius=0.055, color='darkgrey')
 
+        # adding each shape to a patch so that it can be displayed
         self.ax.add_patch(self.body)
         self.ax.add_patch(self.centerPt)
         self.ax.add_patch(self.frontTriangle)
@@ -189,6 +200,9 @@ class Robot:
         self.ax.add_patch(self.eyel)
         self.ax.add_patch(self.pupilr)
         self.ax.add_patch(self.pupill)
+
+        # if we are not showing a diffy swerve robot, the shapes
+        # unique to the diffy are hidden
         if not self.isdiffy:
             self.frontTriangle.set_visible(False)
             self.wheelHolefr.set_visible(False)
@@ -199,23 +213,41 @@ class Robot:
     # --- Helpers --- #
     # --- Drawing and Updates --- #
     def update(self):
-        self.velocity = self.maxVelocity if self.velocity > self.maxVelocity else self.velocity
-        self.velocityAngle += 3
-        self.dx = self.velocity * math.sin(math.radians(-self.velocityAngle))
-        self.dy = self.velocity * math.cos(math.radians(self.velocityAngle))
-        self.x += self.dx
-        self.y += self.dy
+        """
+        Updates the position and heading, velocities, and accelerations
+        This function does not draw the robot, the draw function does that.
+        This function is called by the draw function before drawing the robot.
+        """
+        # idk what to do with acceleration, but there will probably be some acceleration function
+        # based off of velocity or maybe its changed in a goTo function or something similar (?)
+        self.acceleration = 0
+        self.turnAcceleration = 0
 
+        self.velocity = min(self.velocity + self.acceleration, self.MAX_VELOCITY)
+        self.turnVelocity = math.copysign(min(abs(self.turnVelocity + self.turnAcceleration), self.MAX_TURN_VELOCITY),
+                                          self.turnVelocity)
+        # turn velocity is how much the velocity angle changes by
+        self.velocityAngle += self.turnVelocity
+
+        # changing the robot position
+        self.x += self.velocity * math.sin(math.radians(-self.velocityAngle))
+        self.y += self.velocity * math.cos(math.radians(self.velocityAngle))
+
+        # going to add velocity and acceleration to this later
         self.heading += 0
 
     def draw(self, ax):
         """
         Calls the update function, and draws the robot, either with a holonomic or differential swerve drivetrain.
+        It updates all the shapes positions and attributes.
+        The isdiffy variable controls if a differential swerve or holonomic drivetrain is drawn.
 
         Args:
             ax: the axis to add patches to so the shapes can be drawn
         """
         self.update()
+
+        # hopefully no one including me should have to go through this
         if not self.isdiffy:
             self.body.set(xy=(self.x - 0.15 * self.scaling, self.y - 0.25 * self.scaling),
                           width=0.30 * self.scaling,
@@ -284,36 +316,36 @@ class Robot:
             # r=right, l=left, f=front, b=back
             self.wheelHolefr.set(
                 center=(self.x + 0.24234531148 * math.cos(math.radians(self.heading + 45)) * self.scaling,
-                        self.y + 0.24234531148 * math.sin(
-                            math.radians(self.heading + 45)) * self.scaling),
+                        self.y + 0.24234531148 * math.sin(math.radians(self.heading + 45)) * self.scaling),
                 radius=0.055 * self.scaling, visible=True)
             self.wheelHolebr.set(
                 center=(self.x + 0.24234531148 * math.cos(math.radians(self.heading + 315)) * self.scaling,
-                        self.y + 0.24234531148 * math.sin(
-                            math.radians(self.heading + 315)) * self.scaling),
+                        self.y + 0.24234531148 * math.sin(math.radians(self.heading + 315)) * self.scaling),
                 radius=0.055 * self.scaling, visible=True)
             self.wheelHolefl.set(
                 center=(self.x + 0.23234531148 * math.cos(math.radians(self.heading + 133)) * self.scaling,
-                        self.y + 0.23234531148 * math.sin(
-                            math.radians(self.heading + 133)) * self.scaling),
+                        self.y + 0.23234531148 * math.sin(math.radians(self.heading + 133)) * self.scaling),
                 radius=0.055 * self.scaling, visible=True)
             self.wheelHolebl.set(
                 center=(self.x + 0.23234531148 * math.cos(math.radians(self.heading + 226)) * self.scaling,
-                        self.y + 0.23234531148 * math.sin(
-                            math.radians(self.heading + 226)) * self.scaling),
+                        self.y + 0.23234531148 * math.sin(math.radians(self.heading + 226)) * self.scaling),
                 radius=0.055 * self.scaling, visible=True)
-            self.wheelfr.set(xy=(self.x + 0.1875 * self.scaling, self.y + (0.12) * self.scaling),
-                             width=-0.05 * self.scaling, angle=self.velocityAngle,
+            self.wheelfr.set(xy=(self.x + (0.2351728088 * math.cos(math.radians(self.heading + 46.3)) - 0.018) * self.scaling,
+                                 self.y + (0.2351728088 * math.sin(math.radians(self.heading + 46.3)) - 0.0475) * self.scaling),
+                             width=0.05 * self.scaling, angle=self.velocityAngle,
                              height=0.1 * self.scaling, lw=None, fill=True, visible=True)
-            self.wheelbr.set(xy=(self.x + 0.1875 * self.scaling, self.y - (0.12) * self.scaling),
-                             width=-0.05 * self.scaling, angle=self.velocityAngle,
-                             height=-0.1 * self.scaling, lw=None, fill=True)
-            self.wheelfl.set(xy=(self.x - 0.1875 * self.scaling, self.y + (0.12) * self.scaling),
+            self.wheelbr.set(xy=(self.x + (0.2351728088 * math.cos(math.radians(self.heading + 316.3)) - 0.025) * self.scaling,
+                                 self.y + (0.2351728088 * math.sin(math.radians(self.heading + 316.3)) - 0.0575) * self.scaling),
                              width=0.05 * self.scaling, angle=self.velocityAngle,
                              height=0.1 * self.scaling, lw=None, fill=True)
-            self.wheelbl.set(xy=(self.x - 0.1875 * self.scaling, self.y - (0.12) * self.scaling),
+            self.wheelfl.set(xy=(self.x + (0.2351728088 * math.cos(math.radians(self.heading + 133.7)) - 0.02425) * self.scaling,
+                                 self.y + (0.2351728088 * math.sin(math.radians(self.heading + 133.7)) - 0.0475) * self.scaling),
                              width=0.05 * self.scaling, angle=self.velocityAngle,
-                             height=-0.1 * self.scaling, lw=None, fill=True)
+                             height=0.1 * self.scaling, lw=None, fill=True)
+            self.wheelbl.set(xy=(self.x + (0.2351728088 * math.cos(math.radians(self.heading + 226.3)) - 0.024725) * self.scaling,
+                                 self.y + (0.2351728088 * math.sin(math.radians(self.heading + 226.3)) - 0.046) * self.scaling),
+                             width=0.05 * self.scaling, angle=self.velocityAngle,
+                             height=0.1 * self.scaling, lw=None, fill=True)
             self.eyer.set(
                 center=(self.x + 0.09848857801 * self.scaling * math.cos(math.radians(self.heading + 24)), self.y +
                         0.09848857801 * self.scaling * math.sin(math.radians(self.heading + 24))),
@@ -355,7 +387,7 @@ def updateFrame(frame, ax, robot: Robot, trailX, trailY, trail):
     trail.set_data(trailX, trailY)
 
     # the robot draws itself
-    return robot.draw(ax) + (trail,)
+    return (trail,) + robot.draw(ax)
 
 
 # --------------- MAIN --------------- #
@@ -363,8 +395,12 @@ def updateFrame(frame, ax, robot: Robot, trailX, trailY, trail):
 # calls the functions which actually does stuff
 # the function is literally useless, but I hold onto my c++ roots
 def main():
-    animation = Animation()
-    animation.display()
+    # kinda rhymes
+    if __name__ == "__main__":
+        animation = Animation()
+        animation.display()
+    else:
+        exit("Not main Python file.")
 
 
 # I do not embrace some python aspects
